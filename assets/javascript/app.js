518509b11c514c7737b8ac7cea6f214185937098
Prince0fTime <prince0ftime.github@gmail.com>
2019-01-26 10:21:39 -0600

    added asset files

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    console.log("start");
+
+    var player1 = {
+        HP: 500,
+        AP: 10,
+        SP: 15,
+        p1Pos
+    }
+    var player2 = {
+        HP: 500,
+        AP: 10,
+        SP: 15,
+        p2Pos
+    }
+   
+    
+    // var PlayerArray = [player1, player2];
+
+
+
+});
