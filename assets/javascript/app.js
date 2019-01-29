89b646c011f3efbf08eb839cf08160b251b427e6
Prince0fTime <prince0ftime.github@gmail.com>
2019-01-28 19:00:05 -0600

    added some code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,14 +3,46 @@ $(document).ready(function () {
 
 
 
-    var p1PosLeft;
-    var p1PosTop;
+    var p1Pos;
     var p1 = $('.player1');
+
+    var p2Pos;
+    var p2 = $('.player2');
+
+    p1Pos = p1.offset();
+    p2Pos = p2.offset();
+
+
+    var Colision = function () {
+        if (p2Pos.left - p1Pos.left <= 80 && p2Pos.left - p1Pos.left >= -80) {
+
+        } else {
+            console.log(false);
+
+            return (false);
+
+        }
+        console.log(true);
+
+        return (true);
+
+    };
     // -----moves------
 
     function punch() {
+        console.log("punch");
         p1.addClass(className);
         soundManager.play('huh1');
+        if (Colision() == true) {
+            soundManager.play('hit1');
+            p2.addClass('hit1');
+            setTimeout(function () {
+                p2.removeClass('hit1');
+            }, 500);
+        }
+        setTimeout(function () {
+            p1.removeClass('punch');
+        }, 150);
 
     }
 
@@ -20,7 +52,7 @@ $(document).ready(function () {
 
     }
 
-    function rkick() {
+    function rekick() {
         p1.addClass(className);
         soundManager.play('huh2');
 
